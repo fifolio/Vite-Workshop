907b2523e491b30073205b61c3c73a0094bf113c
Firas <firasdabbabi@gmail.com>
2023-03-18 20:41:21 +0100

    version 1.2: Add default server port

--- VITE/vite.config.js
+++ VITE/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Add default server port
   server: {
     port: 3000,
   },
